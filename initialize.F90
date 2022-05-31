898e69b4d541f0b338b83b0e6f8454a9d882720e
sankalpjena <sankalp1996raja@gmail.com>
2022-05-31 15:17:27 +0200

    basic program to initialize the particles

--- initialize.F90
+++ initialize.F90
@@ -6,7 +6,9 @@ real :: positions(2), x, y
 Np = 1 
 allocate(P(Np))
 positions = 2
-
+! use of random_number: https://masuday.github.io/fortran_tutorial/random.html
+! note that the random number(s) are stored in the variable that is passed
+! to the generator and are of the same type as of the variable
 do i=1,Np
   call random_number(p(i)%X)
   p(i)%m = 1
